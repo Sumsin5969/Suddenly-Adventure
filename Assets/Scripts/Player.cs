c77a545b07fce40d9989400551dcf525a8c01c45
jcj154248 <jcj154248@naver.com>
2023-09-24 18:26:51 +0900

    Walking_FINAL

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,41 @@ public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
     Rigidbody2D rigid;
-    
-    // Start is called before the first frame update
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        // 마찰력
+        if(Input.GetButtonUp("Horizontal")) 
+        {
+            rigid.velocity = new Vector2 (rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        // 방향 전환
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+        // 무빙 애니메이션
+        if(Mathf.Abs(rigid.velocity.x) < 0.2)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+        }
+        
+    }
+
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal"); // 키보드 입력값
