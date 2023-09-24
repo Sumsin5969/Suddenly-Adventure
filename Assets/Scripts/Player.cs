e274228f8d406a395fbc9b06c9528ec6337a3068
jcj154248 <jcj154248@naver.com>
2023-09-24 15:35:32 +0900

    PlayerMove

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid; 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+    }
+}
