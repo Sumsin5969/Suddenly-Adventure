951e3731c967649d797b3693e56abe6f1173830e
jcj154248 <jcj154248@naver.com>
2023-12-09 19:20:03 +0900

    카메라 범위 제한 (마을, 던전2-3 만 범위값 넣어놈)

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -8,7 +8,7 @@ public class Camera : MonoBehaviour
     public GameObject target; // 카메라가 따라갈 대상
     public float moveSpeed; // 카메라가 따라갈 속도
     private Vector3 targetPosition; // 대상의 현재 위치
-
+    public float minX, maxX, minY, maxY; // 제한 범위
 
     void Update()
     {
@@ -20,6 +20,17 @@ public class Camera : MonoBehaviour
 
             // vectorA -> B까지 T의 속도로 이동
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+
+        if (target != null)
+        {
+            // 대상의 위치에 따라 카메라의 목표 위치 설정
+            targetPosition.Set(Mathf.Clamp(target.transform.position.x, minX, maxX),
+                                Mathf.Clamp(target.transform.position.y + 4f, minY, maxY),
+                                transform.position.z);
+
+            // 목표 위치로 부드럽게 이동
+            transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
     }
 }
