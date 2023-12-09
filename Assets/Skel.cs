db2da3a2e5d1cda8d5bb35ec647cf0b736f58079
jcj154248 <jcj154248@naver.com>
2023-12-10 06:41:11 +0900

    스켈레톤 방어 기능

--- Assets/Skel.cs
+++ Assets/Skel.cs
@@ -15,6 +15,9 @@ public class Skel : MonoBehaviour
 
     public int enemyhealth;
 
+    private bool isInvincible = false;
+    public float invincibilityTime; // 무적 지속 시간
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -25,24 +28,43 @@ public class Skel : MonoBehaviour
 
     public void EnemyHit(int damage)
     {
-        enemyhealth -= damage;
-
-        if (enemyhealth <= 0) // 적 사망
+        if (!isInvincible) // 방어 중이 아닐 때
         {
-            anim.SetTrigger("Death");
-            anim.Play("Death", 0);
-            gameObject.layer = 14;
-        }
-        else // 적 피격
-        {
-            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-            Transform playerTransform = playerObject.transform;
-            anim.SetTrigger("Hit");
-            anim.Play("Hit", 0);
+            enemyhealth -= damage;
 
-            int add = transform.position.x - playerTransform.position.x > 0 ? 1 : -1;
-            rigid.AddForce(new Vector2(add, 0.5f) * 2, ForceMode2D.Impulse);
+            if (enemyhealth <= 0) // 적 사망
+            {
+                anim.SetTrigger("Death");
+                anim.Play("Death", 0);
+                gameObject.layer = 14;
+            }
+            else // 적 피격
+            {
+                GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+                Transform playerTransform = playerObject.transform;
+                anim.SetTrigger("Hit");
+                anim.Play("Hit", 0);
+
+                int add = transform.position.x - playerTransform.position.x > 0 ? 1 : -1;
+                rigid.AddForce(new Vector2(add, 0.5f) * 2, ForceMode2D.Impulse);
+
+                // 방어 애니메이션 재생 및 무적 상태 설정
+                StartCoroutine(DefendCooldown());
+            }
         }
+    }
+
+    IEnumerator DefendCooldown()
+    {
+        anim.SetTrigger("Defense");
+        isInvincible = true;    
+        yield return new WaitForSeconds(invincibilityTime);
+
+        // 무적이 끝난 후에는 방어 기능을 다시 비활성화
+        isInvincible = false;
+
+        // 중복 코루틴 방지
+        StopCoroutine(DefendCooldown());
     }
 
     public void OffDamaged()
